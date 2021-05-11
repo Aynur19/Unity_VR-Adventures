ae0be27b153ad545f92c1fe851c2f1a7044699ed
Aynur19 <nasybullin.aynur@yandex.ru>
2021-05-11 15:11:32 +0300

    import Oculus Integration

---
+++ Assets/Scripts/SwordController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+    //Quaternion defaultRotation;
+
+    //void Awake()
+    //{
+    //    defaultRotation = transform.rotation;
+    //}
+
+    //void LateUpdate()
+    //{
+    //    transform.rotation = defaultRotation;
+    //}
+}
